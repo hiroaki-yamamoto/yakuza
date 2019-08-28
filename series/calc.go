f0718721d7e184bc7ad63f0fdbc49a80a61c6c08
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-08-28 12:27:24 +0900

    Dropped fixed precision support.
    In this world, BIG contains small.

---
+++ series/calc.go
@@ -0,0 +1,13 @@
+package series
+
+import yak "github.com/hiroaki-yamamoto/yakuza"
+
+// Calculation code for series
+
+// Add calculates the addtion of a and b and returns the result.
+func Add(
+	a, b yak.ICalculableSeries,
+	fillValue interface{},
+) yak.ICalculableSeries {
+	return nil
+}
--- series/map.go
+++ series/map.go
@@ -5,26 +5,8 @@ import "math/big"
 // GeneralMapSeries represents a map with string key and interface value.
 type GeneralMapSeries map[string]interface{}
 
-// IntMapSeries represents a map with string key and int value.
-type IntMapSeries map[string]int
-
-// Int16MapSeries represents a map with string key and int16 value.
-type Int16MapSeries map[string]int16
-
-// Int32MapSeries represents a map with string key and int32 value.
-type Int32MapSeries map[string]int32
-
-// Int64MapSeries represents a map with string key and int64 value.
-type Int64MapSeries map[string]int64
-
 // BigIntMapSeries represents a map with string key and big.Int value
 type BigIntMapSeries map[string]*big.Int
-
-// Float32MapSeries represents a map with string key and float32 value.
-type Float32MapSeries map[string]float32
-
-// Float64MapSeries represents a map with string key and float64 value.
-type Float64MapSeries map[string]float64
 
 // BigFloatMapSeries represents a map with string key and big.Float value.
 type BigFloatMapSeries map[string]*big.Float
@@ -34,23 +16,11 @@ type BigRatMapSeries map[string]*big.Rat
 
 // Implement ISeries
 func (s *GeneralMapSeries) series()  {}
-func (s *IntMapSeries) series()      {}
-func (s *Int16MapSeries) series()    {}
-func (s *Int32MapSeries) series()    {}
-func (s *Int64MapSeries) series()    {}
 func (s *BigIntMapSeries) series()   {}
-func (s *Float32MapSeries) series()  {}
-func (s *Float64MapSeries) series()  {}
 func (s *BigFloatMapSeries) series() {}
 func (s *BigRatMapSeries) series()   {}
 
 // Implement ICalculable
-func (s *IntMapSeries) calculable()      {}
-func (s *Int16MapSeries) calculable()    {}
-func (s *Int32MapSeries) calculable()    {}
-func (s *Int64MapSeries) calculable()    {}
 func (s *BigIntMapSeries) calculable()   {}
-func (s *Float32MapSeries) calculable()  {}
-func (s *Float64MapSeries) calculable()  {}
 func (s *BigFloatMapSeries) calculable() {}
 func (s *BigRatMapSeries) calculable()   {}
