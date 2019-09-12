5efc428b13f8ffc56e299b27065dbbfde764d6ac
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-09-12 11:00:29 +0900

    Adding mid-lack case, but somehow I don't get motivated now... :(

--- series/calc_test.go
+++ series/calc_test.go
@@ -23,16 +23,35 @@ var _ = Describe("Normal Series", func() {
 					arr2 := make(series.BigIntSeries, sz)
 					sol := make(series.BigIntSeries, sz)
 					Context("Full filled with random integers", func() {
-						for i := 0; i < sz; i++ {
-							arr1[i] = big.NewInt(random.Int63())
-							arr2[i] = big.NewInt(random.Int63())
-							sol[i] = big.NewInt(0).Add(arr1[i], arr2[i])
-						}
-						It("should successfully and correct result.", func() {
+						BeforeEach(func() {
+							for i := 0; i < sz; i++ {
+								arr1[i] = big.NewInt(random.Int63())
+								arr2[i] = big.NewInt(random.Int63())
+								sol[i] = big.NewInt(0).Add(arr1[i], arr2[i])
+							}
+						})
+						It("should be a correct result.", func() {
 							result := series.Add(arr1, arr2, nil)
 							Expect(result.(series.BigIntSeries)).To(Equal(sol))
 						})
 					})
+
+					// Context("filled with nil at several opA's elems", func() {
+					// 	BeforeEach(func() {
+					// 		for i := 0; i < sz; i++ {
+					// 			arr1[i] = big.NewInt(random.Int63())
+					// 			if (random.Int() & 0x01) == 0x01 {
+					// 				arr1[i] = nil
+					// 			} else {
+					// 				arr1[i] = big.NewInt(random.Int63())
+					// 			}
+					// 			arr2[i] = big.NewInt(random.Int63())
+					// 			sol[i] = big.NewInt(0).Add(arr1[i], arr2[i])
+					// 		}
+					// 	})
+					// 	Context("without fillValue", func(){
+					// 	})
+					// })
 				}
 			}
 			Context(
